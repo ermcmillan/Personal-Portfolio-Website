e392f2fa1b2615674b5267ce76e98ac499ac8eb6
Elizabeth Mcmillan <ermcmillan@crimson.ua.edu>
2020-11-12 10:36:32 -0600

    Add project files.

---
+++ Pages/Skills.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PersonalPortfolioSite.Pages
+{
+    public class SkillsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
